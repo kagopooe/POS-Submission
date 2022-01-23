e10a593dda66f413be58a92e071f8118227d073c
kagopooe <91526443+kagopooe@users.noreply.github.com>
2022-01-23 12:31:58 +0200

    Update
    Error in old code

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -1,117 +1,77 @@
-const shoppingCart = function(){
+        // Cost of all products in the cart
+        var total = 0;
+        // Index
+        var i = 1;
+        // Message 
+        var message = "Please select a size";
+        // List of the amount of every product in the cart
+        var itemCost = [];
 
-}
-
-let prod1 = document.querySelector(".players")
-    cart1 = document.querySelector(".cart-list")
-    productQ = document.querySelector(".quantity")
-    mpTcart = document.querySelector(".btn-cart-clear")
-    checkO = document.querySelector(".btn-cart-check")
-    totalP = document.querySelector(".total-price")
-;
-
-let players = [
-        {
-          id: 0,
-          name: "Aaron Ramsdale",
-          description: "Goalkeeper, Arsenal FC & England.",
-          imageUrl: "https://postimg.cc/8s3Nnmdw",
-          price: "60 000 000"
-        }
-],
-
-
-// data array here
-playersInCart = [];
-
-const listPlayers = function () {
-    players.forEach(function(players) {
-        let player = document.createElement('div');
-        player.className = "player";
-        player.innerHTML = `
-                            <div class="player-img"> <img src = "${players.imageUrl}"</div>
-                            <div class="playerName"><span>Player:</span> ${players.name}</div>
-                            <div class""><span>Description:</span> ${players.description}</div>
-                            <div class='price'><span>Price:</span> ${players.price} <span>&pound;</span></div>
-                            <div class='add-to-cart'><a href"" class="btn moreinfo">More Details</a>
-                            <a class"btn add-to-cart" data-id=${players.id}>Add to cart</a>
-                            </div></div>
-                            `;
-                            prod1.appendChild(player);
-                        });
-                    }
-    const generateCartList = function() {
-        cart1.innerHTML = "";
-        playersInCart.forEach(function(players) {
-            let li = document.createElement("li");
-            li.innerHTML = `
-                            ${players.quantity} ${players.player.name} - <span>&pound;</span>${players.price * players.quantity}`;
-                            cart1.appendChild(li);
-        }); 
-        productQ.innerHTML = playersInCart.length;
-        generateCartButtons()}
-
-//empty cart
-const generateCartButtons = function() {
-    if (playersInCart.length > 0) {
-        mpTcart.style.display = "block";
-        checkO.style.display = "block"
-        totalP.innerHTML = "<span>&pound;</span> " + calculateTotalPrice();
-    }
-    else {
-        mpTcart.style.display = "none";
-        checkO.style.display  = "none" 
-     }  
-}
-        
-const setupListeners = function() {
-    prod1.addEventListener("click", function(event) {
-        let project = event.target;
-        if(project.classList.contains("add-to-cart")) {
-            let projectId = project.dataset.id;
-            addToCart(projectId)
-        }})
-    mpTcart.addEventListener("click", function(event) {
-        if(confirm("Are you sure?")) {
-            playersInCart = [];
-        }
-        generateCartList();
-    });
-} 
-
-const addToCart = function(id) {
-    let obj = players[id];
-    if(playersInCart.length === 0 || playerFound(obj.id)=== undefined) {
-        playersInCart.push({player: obj, quantity: 1});}
-    else {
-        playersInCart.forEach(function(players) {
-            if(players.id === obj.id) {
-                players.quantity++;
+        // Add to cart
+        function add(n){
+            // n is the rank of the T-shirt
+            sizeId = "size" + n;
+            /* Each shirt has Id start with a root + his rank (Ex: Size id for product 1 is size1,
+             price id for product 3 is price 3 ...)*/
+            messageId = "message" + n;
+            // The size of the selected shirt
+            size = document.getElementById(sizeId).value;
+            // If the client don't select size, a message will show up
+            if (size==="size"){
+                document.getElementById(messageId).innerHTML = message;
+                return 0;
             }
-        });
-    }
-    generateCartList();}
+            // Getting all Id of the selected shirt(brand ex: nike, price and quantity)
+            brand = "brand" + n;
+            priceId = "price" + n;
+            quantityId = "quantity" + n;
+            // Getting details of the selected shirt
+            // brand
+            name = document.getElementById(brand).innerHTML;
+            // price
+            price = document.getElementById(priceId).innerHTML;
+            // quantity
+            quantity = document.getElementById(quantityId).value;
+            // Creating a li element to add it to ul 
+            var node = document.createElement("LI");
+            // id of li element
+            item = "item"+i;
+            node.setAttribute("id", item)
+            // cost of the selected shirt
+            itemCost[i-1] = Number(price) * Number(quantity);
+            // Updating the index i 
+            i += 1;
+            // text of the li element
+            var textnode = document.createTextNode(name+" "+quantity+" x R "+price+" , size: "+ size);
+            // add the text to li element
+            node.appendChild(textnode);
+            // add li element to ul list
+            document.getElementById("items").appendChild(node);
 
-let playerFound = function(playerId) {
-    return playersInCart.find(function(players)  {
-        return players.id === playerId;
-    });    
-}
+            total += Number(price) * Number(quantity);
+            // update the total
+            document.getElementById("total").innerHTML = "Total: " + " R " + total.toFixed(2);
+            
+            // Add a remove button 
+            document.getElementById(item).innerHTML += '<button onclick="deleItem('+"'"+item+"'"+')">x</button>';
+            // you have to respect the order of: '' and ""
+            
+        }
 
-let calculateTotalPrice = function () {
-    return playersInCart.reduce(function(total,players) {
-        return total + (players.price * players.quantity);
-    }, 0);
-}
+         // delete message when the select element is clicked
+         function deleteE(eId) {
+            document.getElementById(eId).innerHTML = ' ';
+        }
 
-let init = function() {
-    generatePlayerList();
-    setupListeners();
-}
-return {
-    init: init
-}
-    
-    
-    
-
+        // Remove a product from the cart
+        function deleItem(eId){
+            document.getElementById(eId).remove();
+            // slice is string method
+            // eId (element Id) contain root + number (ex: item4)
+            // n is the number in eId 
+            n = Number(eId.slice(-1)) - 1;
+            // remove the cost of the product deleted from the cart
+            total -= itemCost[n];
+            // Updating the cost of products in the cart
+            document.getElementById("total").innerHTML = "Total: " + " R" + total.toFixed(2) ; 
+        }
